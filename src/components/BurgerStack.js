66bc9692a6fd6c88d62dff2d869062114f093543
Gabbytam <gtam2@fordham.edu>
2020-12-03 12:02:30 -0800

    added a little styling

--- src/components/BurgerStack.js
+++ src/components/BurgerStack.js
@@ -9,7 +9,7 @@ const BurgerStack = ({builtBurger}) => {
         <div>
             {/* going to have to map through the array of builtBurger and print render them in order */}
             {builtBurger.map(topping => (
-                <Ingredient name={topping.name} color={topping.color}/>
+                <Ingredient name={topping.name} color={topping.color} status='built'/>
             ))
             }
             {console.log('this is builtBurger', builtBurger)}
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+// CSS imports
+import '../css/components/ClassComponents.css';
+
 //this Ingredient component grabs name and color props and displays/uses it
-const Ingredient = ({name, color, moveOption}) => {
+const Ingredient = ({name, color, moveOption, status}) => {
 
     return(
-        <div>
+        <div class='item'>
             <div style={{color: {color}}}>{name}</div>
             {/* when button is click, run a function that will add it to the new array of builtBurger */}
-            <button onClick= {e => moveOption({name, color})} value={name} >→</button>
+            
+            {/* inline styling will check the status, which is only passed for the built burger call to Ingredient component, and wont display the button if status is truthy */}
+            <button onClick= {e => moveOption({name, color})} style={{display: status ? 'none' : ''}} >→</button> {/*note: could not be written as onClick= {moveOption({name, color} and I dont know why)} */}
         </div>
     )
 }
