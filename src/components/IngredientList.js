21175b59cdd5435aebb535a1728bf8f1d3169775
nichro02 <nichro02@gmail.com>
2020-12-02 18:19:49 -0800

    Added components and set initial state for ingredients

---
+++ src/components/IngredientList.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+//Components
+import Ingredients from './Ingredients'
+
+const IngredientList = () => {
+
+  return <div>FunctionalComponent</div>;
+};
+
+export default IngredientList;
