a6a50533df561b53e0cd4b25629446185329fbb4
granddivino <granddivino@gmail.com>
2020-12-04 22:40:30 -0700

    Completed deliverable with partial bonus

---
+++ src/components/BurgerPane.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import BurgerStack from './BurgerStack';
+import ClearBurger from './ClearBurger';
+
+const BurgerPane = (props) => {
+    return(
+        <div class="list burgerlist">
+            <h1>Here's your Burger 🍔:</h1>
+            <BurgerStack chosenIngredients={props.chosenIngredients} />
+            <ClearBurger clearBurger={props.clearBurger} />
+        </div>
+    )
+}
+
+export default BurgerPane;
